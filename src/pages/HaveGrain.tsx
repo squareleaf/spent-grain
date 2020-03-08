c8883078ff49fd8885364ee9ce3a13a51cd646be
squareleaf <tiffanydgreen@gmail.com>
2020-03-08 13:52:52 -0500

    Start adding content to have grain page

--- src/pages/HaveGrain.tsx
+++ src/pages/HaveGrain.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './HaveGrain.css';
 
@@ -18,6 +18,33 @@ const HaveGrain: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="I have grain page" />
+        <IonGrid>
+          <IonRow>
+            <IonCol>Your name (optional)</IonCol>
+            <IonCol>Name input</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>Street address</IonCol>
+            <IonCol>Street address input</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>City</IonCol>
+            <IonCol>City input</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>Where will the grain be? (driveway, porch, curb, etc)</IonCol>
+            <IonCol>Grain location input</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>Any grain details? (type of grain, adjuncts mixed in, etc)</IonCol>
+            <IonCol>Grain details input</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>Any pickup details? ("Pick up before 6pm", "Do not ring the bell")</IonCol>
+            <IonCol>Pickup details input</IonCol>
+          </IonRow>
+        </IonGrid>
+
       </IonContent>
     </IonPage>
   );
