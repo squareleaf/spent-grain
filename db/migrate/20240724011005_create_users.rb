2b0688af21c4c0e1027a588a9d075782aca7d294
squareleaf <tiffanydgreen@gmail.com>
2024-08-25 20:03:44 -0500

    Add rspec, remove capybara testing. Write basic model validity tests

--- db/migrate/20240724011005_create_users.rb
+++ db/migrate/20240724011005_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :street_address
       t.string :city
       t.string :state
